dfbe9832baaaa488955f68b63edb98dad80332e6
Kemail-terminal <sulkemref@gmail.com>
2023-08-05 22:47:35 -0500

    CheckOut done

--- checkout/src/main/java/com/cydeo/checkout/controller/CheckoutController.java
+++ checkout/src/main/java/com/cydeo/checkout/controller/CheckoutController.java
@@ -1,6 +1,8 @@
 package com.cydeo.checkout.controller;
 
 import com.cydeo.checkout.dto.CheckoutDTO;
+import com.cydeo.checkout.service.ProducerService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/checkout")
 public class CheckoutController {
 
-    @PostMapping
-    public String checkout(@RequestBody CheckoutDTO checkoutDTO){
+    private final ProducerService producerService;
 
+    public CheckoutController(ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @PostMapping
+    public String checkout(@RequestBody CheckoutDTO checkoutDTO) throws JsonProcessingException {
+        producerService.sendMessage(checkoutDTO);
         return "message sent";
     }
 }
--- checkout/src/main/java/com/cydeo/checkout/service/CheckoutService.java
+++
@@ -1,7 +0,0 @@
-package com.cydeo.checkout.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CheckoutService {
-}
